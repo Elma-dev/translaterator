6bf4f4d835552acb9547c81a861f3ddf13ffb396
abdeljalil el majjodi <aelmajjodi@abdeljalils-MacBook-Air.local>
2026-04-18 15:54:44 +0100

    add nllb model n

---
+++ translator_train/nllb_ft/configs.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+MODEL_ID = "facebook/nllb-200-distilled-600M"
+DATASET_ID = "abdeljalilELmajjodi/darija_english_translation_new"
+DATASET_SPLIT = "train"
+SRC_LANG = "eng_Latn"
+TGT_LANG = "ary_Arab"
+FREEZE_ENCODER = True
+ENGLISH_COLUMN = "english"
+DARIJA_COLUMN = "darija"
+MAX_LENGTH = None
+TEST_SIZE = 0.1
+OUTPUT_DIR = ""
+TRAIN_BATCH_SIZE = 8
+TEST_BATCH_SIZE = 8
+LOG_STEPS = 100
+EVAL_STEPS = 1000
+LEARNING_RATE = 2e-5
+W_DECAY = 0.02
+RUN_NAME = f"{MODEL_ID.split('/')[-1]}-lr{LEARNING_RATE}-bs{TRAIN_BATCH_SIZE}-{datetime.now().strftime('%m%d-%H%M')}"
+SAVE_STEPS = 100
+SAVE_TOTAL_LIMIT = 2
---
+++ translator_train/nllb_ft/train.py
@@ -0,0 +1,74 @@
+from transformers import (
+    AutoModelForSeq2Seq,
+    AutoTokenizer,
+    Seq2SeqTrainingArguments,
+    Seq2SeqTrainer,
+    DataCollatorWithPadding,
+)
+from datasets import load_dataset, Dataset
+from configs import *
+
+
+def tokenize_dataset(dataset, tokenizer):
+    english_token_ids = tokenizer(
+        dataset[ENGLISH_COLUMN], padding=True, truncation=True, max_length=MAX_LENGTH
+    )
+
+    darija_token_ids = tokenizer(
+        dataset[DARIJA_COLUMN], padding=True, truncation=True, max_length=MAX_LENGTH
+    )
+
+    final_dataset = Dataset.from_list(
+        {
+            "input_ids": english_token_ids["input_ids"],
+            "attention_mask": english_token_ids["attention_mask"],
+            "labels": darija_token_ids["input_ids"],
+        }
+    )
+    return final_dataset
+
+
+if __name__ == "__main__":
+    model = AutoModelForSeq2Seq.from_pretrained(MODEL_ID)
+    tokenizer = AutoTokenizer.from_pretrained(
+        MODEL_ID, src_lang=SRC_LANG, tgt_lang=TGT_LANG
+    )
+
+    # freeze encoder
+    if FREEZE_ENCODER:
+        for params in model.model.encode.parameters():
+            params.requires_grad = False
+
+    # load dataset
+    dataset = load_dataset(DATASET_ID, split=DATASET_SPLIT)
+    dataset_split = dataset.train_test_split(test_size=TEST_SIZE, seed=42)
+    test, train = dataset_split["test"], dataset_split["train"]
+    test = tokenize_dataset(test, tokenizer)
+    train = tokenize_dataset(train, tokenizer)
+
+    # prepare Trainer
+    training_args = Seq2SeqTrainingArguments(
+        output_dir=OUTPUT_DIR,
+        per_device_train_batch=TRAIN_BATCH_SIZE,
+        per_device_eval_batch=TEST_BATCH_SIZE,
+        logging_steps=LOG_STEPS,
+        evaluation_strategy="steps",
+        eval_steps=EVAL_STEPS,
+        num_train_epochs=3,
+        learning_rate=LEARNING_RATE,
+        weight_decay=W_DECAY,
+        report_to="trackio",
+        predict_with_generate=True,
+        run_name=RUN_NAME,
+        save_steps=SAVE_STEPS,
+        save_total_limit=SAVE_TOTAL_LIMIT,
+    )
+
+    trainer = Seq2SeqTrainer(
+        model=model,
+        tokenizer=tokenizer,
+        args=training_args,
+        train_dataset=train,
+        eval_dataset=test,
+        data_collator=DataCollatorWithPadding(tokenizer=tokenizer),
+    )
